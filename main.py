33dcf1eaf988a23a4207241e688ccc0a8d8ac9e9
Victor Hugo D. Magalhães <hello@victordias.dev>
2021-09-22 21:20:50 -0300

    adding scripts

---
+++ main.py
@@ -0,0 +1,31 @@
+# coding: utf8
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from fpdf import FPDF
+
+
+def generatePDF(title, body):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.add_font('Arial', '', './arial.ttf', uni=True)
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt=title, ln=1, align='C')
+    pdf.multi_cell(185, 5, body, 0, 'J', 0, False)
+    pdf.output("receita.pdf").encode('utf-8', 'ignore')
+
+
+def main():
+    recipe_url = str(input("link da receita: "))
+    html = urlopen(recipe_url)
+    bs = BeautifulSoup(html, 'html.parser')
+    title = bs.find("h1").find(text=True)
+    recipe = bs.find("div", {"class": "wg-text container"}).find_all('p')
+    article_text = ''
+    for line in recipe[:-2]:
+        article_text += '\n' + ''.join(line.findAll(text=True))
+
+    generatePDF(title, article_text)
+
+
+if __name__ == "__main__":
+    main()
